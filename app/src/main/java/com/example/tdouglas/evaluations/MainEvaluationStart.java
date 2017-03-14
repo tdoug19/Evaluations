9a5bc117f2e96ef198cb606b96205a71c0c322b9
tdoug19 <tdoug19@gmail.com>
2017-03-14 14:26:45 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tdouglas/evaluations/MainEvaluationStart.java
@@ -0,0 +1,13 @@
+package com.example.tdouglas.evaluations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainEvaluationStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_evaluation_start);
+    }
+}
